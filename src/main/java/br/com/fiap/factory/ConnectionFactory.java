8bfb72f4d4bb8d4723440806afac991908a7d187
Marcelo Esmidre <Marceloesmidre1@gmail.com>
2026-04-22 21:10:25 -0300

    correção facotry

--- src/main/java/br/com/fiap/factory/ConnectionFactory.java
+++ src/main/java/br/com/fiap/factory/ConnectionFactory.java
@@ -4,10 +4,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    // Formato: jdbc:oracle:thin:@servidor:porta/servico
-    private static final String URL = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
-    private static final String USUARIO = "marcelo";
-    private static final String SENHA = "marcelo0";
+    private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
+    private static final String USUARIO = "rm567830";
+    private static final String SENHA = "020506";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
