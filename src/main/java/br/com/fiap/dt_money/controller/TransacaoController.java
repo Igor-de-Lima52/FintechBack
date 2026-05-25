fa9554a6331d1c58d643dddecd786576df3cdb02
Ali Salman <bolihsalman@gmail.com>
2026-05-25 00:33:55 -0300

    TransacaoController

---
+++ src/main/java/br/com/fiap/dt_money/controller/TransacaoController.java
@@ -0,0 +1,51 @@
+package br.com.fiap.dt_money.controller;
+
+import br.com.fiap.dt_money.dto.TransacaoRequestDTO;
+import br.com.fiap.dt_money.model.Transacao;
+import br.com.fiap.dt_money.service.TransacaoService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/transacoes")
+public class TransacaoController {
+
+    private final TransacaoService transacaoService;
+
+    public TransacaoController(TransacaoService transacaoService) {
+        this.transacaoService = transacaoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transacao>> listarTodas() {
+        return ResponseEntity.ok(transacaoService.listarTodas());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transacao> buscarPorId(@PathVariable UUID id) {
+        return ResponseEntity.ok(transacaoService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Transacao> criar(@RequestBody @Valid TransacaoRequestDTO dto) {
+        Transacao transacaoCriada = transacaoService.criar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(transacaoCriada);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Transacao> atualizar(@PathVariable UUID id, @RequestBody @Valid TransacaoRequestDTO dto) {
+        Transacao transacaoAtualizada = transacaoService.atualizar(id, dto);
+        return ResponseEntity.ok(transacaoAtualizada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable UUID id) {
+        transacaoService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
