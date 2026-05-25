be5e70f5028ab588698443ed972426327dcac4f2
Ali Salman <bolihsalman@gmail.com>
2026-05-25 00:29:45 -0300

    TransacaoRequestDTO

---
+++ src/main/java/br/com/fiap/dt_money/dto/TransacaoRequestDTO.java
@@ -0,0 +1,28 @@
+package br.com.fiap.dt_money.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.util.UUID;
+
+public record TransacaoRequestDTO(
+
+        @NotBlank(message = "O nome da transação é obrigatório")
+        String nome,
+
+        String descricao,
+
+        @NotNull(message = "O valor é obrigatório")
+        @Positive(message = "O valor deve ser positivo")
+        Double valor,
+
+        @NotNull(message = "O ID da categoria é obrigatório")
+        UUID categoriaId,
+
+        @NotNull(message = "O ID do usuário é obrigatório")
+        UUID usuarioId,
+
+        @NotNull(message = "O ID da conta é obrigatório")
+        UUID contaId
+) {}
